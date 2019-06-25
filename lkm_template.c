a804069d093874eed342190c30cb783f877d090b
Austen Barker <atbarker@ucsc.edu>
2019-06-25 12:30:02 -0700

    added timer and macro

--- lkm_template.c
+++ lkm_template.c
@@ -4,43 +4,51 @@
 #include <linux/rslib.h>
 #include <linux/slab.h>
 #include <linux/random.h>
+#include <linux/timekeeping.h>
 #include "libgfshare.h"
 
-MODULE_LICENSE("MIT");
+#define SECRET_SIZE 4096
+
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("AUSTEN BARKER");
 
 static int __init km_template_init(void){
-    uint8_t* secret = kmalloc(512, GFP_KERNEL);
-    uint8_t* recombine = kmalloc(512, GFP_KERNEL);
-    uint8_t* shard1 = kmalloc(512, GFP_KERNEL);
-    uint8_t* shard2 = kmalloc(512, GFP_KERNEL);
-    uint8_t* shard3 = kmalloc(512, GFP_KERNEL);
+    uint8_t* secret = kmalloc(SECRET_SIZE, GFP_KERNEL);
+    uint8_t* recombine = kmalloc(SECRET_SIZE, GFP_KERNEL);
+    uint8_t* shard1 = kmalloc(SECRET_SIZE, GFP_KERNEL);
+    uint8_t* shard2 = kmalloc(SECRET_SIZE, GFP_KERNEL);
+    uint8_t* shard3 = kmalloc(SECRET_SIZE, GFP_KERNEL);
     uint8_t* sharenrs = "012";
     int i;
+    uint64_t time = 0;
     gfshare_ctx *G;
     gfshare_ctx *G_dec;
 
     printk(KERN_INFO "Inserting kernel module\n");
     
     //populate everything with random bytes
-    get_random_bytes(secret, 512);
+    get_random_bytes(secret, SECRET_SIZE);
     
     printk(KERN_INFO "Splitting randomly generated secret\n");
-    G = gfshare_ctx_init_enc(sharenrs, 3, 2, 512); 
+    time = ktime_get_ns();
+    G = gfshare_ctx_init_enc(sharenrs, 3, 2, SECRET_SIZE); 
     gfshare_ctx_enc_setsecret(G, secret);
     gfshare_ctx_enc_getshare(G, 0, shard1);
     gfshare_ctx_enc_getshare(G, 1, shard2);
     gfshare_ctx_enc_getshare(G, 2, shard3);
+    printk(KERN_INFO "time to split: %lld", ktime_get_ns() - time);
     
     //recombine the secret
-    G_dec = gfshare_ctx_init_dec(sharenrs, 3, 512);
+    time = ktime_get_ns();
+    G_dec = gfshare_ctx_init_dec(sharenrs, 3, SECRET_SIZE);
     gfshare_ctx_dec_giveshare(G_dec, 0, shard1);
     gfshare_ctx_dec_giveshare(G_dec, 1, shard2);
     gfshare_ctx_dec_giveshare(G_dec, 2, shard3);
     gfshare_ctx_dec_extract(G_dec, recombine);
+    printk(KERN_INFO "time to reconstruct: %lld", ktime_get_ns() - time);
     
     //verify the recombination succeeded
-    for(i = 0; i < 512; i++){
+    for(i = 0; i < SECRET_SIZE; i++){
         if(secret[i] != recombine[i]){
              printk(KERN_INFO "Recombine failed at character %d\n", i);
 	     goto exit;
